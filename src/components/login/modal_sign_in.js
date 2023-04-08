10690335715a2a0e606502b17097c5eebe278502
amirnabaei <87382930+amirnabaei@users.noreply.github.com>
2023-04-08 14:18:04 -0700

    add facebook login

--- src/components/login/modal_sign_in.js
+++ src/components/login/modal_sign_in.js
@@ -1,43 +1,55 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import {Grid, Box} from "theme-ui";
 import { signIn, signOut, useSession } from "next-auth/react";
 import GoogleButton from "react-google-button";
-
 
 function ModalWrapper(props) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
- 
+
   const { data: session } = useSession();
-  
 
   return (
     <>
-     {
-        console.log(">>><<>>", session)
-     }
+      {console.log(">>><<>>", session)}
       <Button variant="primary" onClick={handleShow}>
         {props.tile}
       </Button>
-
       <Modal
         show={show}
         onHide={handleClose}
         backdrop="static"
         keyboard={false}
       >
-        <Modal.Header closeButton>
-          <Modal.Title> Sign In.. </Modal.Title>
-        </Modal.Header>
-
-        <GoogleButton
-            onClick={() => signIn("google")}
-            style={{ marginTop: "1rem" }}
-          />
-
+           <Grid gap={1} columns={["1fr 1fr 1fr"]}>
+          <Box></Box>
+          <Box></Box>
+          <Modal.Header closeButton>
+            <Modal.Title>{props.title}</Modal.Title>
+          </Modal.Header>
+          <Box></Box>
+          <Box>
+            <GoogleButton
+              onClick={() => signIn("google")}
+              style={{ marginTop: "1rem" }}
+            />
+          </Box>
+          <Box></Box>
+          <Box></Box>
+          <Box>
+            <Button onClick={() => signIn("facebook")}>
+              Sign in with Facebook
+            </Button>
+          </Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+        </Grid>
       </Modal>
     </>
   );
