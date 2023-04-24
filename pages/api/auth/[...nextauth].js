4d30ea48e8c59a72b9d781d993755757d2b7d00e
anabaei <nabaei17@gmail.com>
2023-04-23 21:21:05 -0700

    add auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,14 +6,48 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId:process.env.CLIENT_ID,
-        clientSecret:process.env.CLIENT_SECRET
-      }),
-      FacebookProvider({
-        clientId: process.env.FACEBOOK_APP_ID,
-        clientSecret: process.env.FACEBOOK_APP_SECRET
-      }),
+      clientId:process.env.CLIENT_ID,
+      clientSecret:process.env.CLIENT_SECRET,
+      accessTokenUrl: 'https://accounts.google.com/o/oauth2/token',
+      requestTokenUrl: 'https://accounts.google.com/o/oauth2/auth',
+      authorization:'https://accounts.google.com/o/oauth2/auth?response_type=code',
+      profileUrl: 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json',
+      // to see what is in profile
+      // profile(profile) {
+      //   return {
+      //     id: profile.id
+      //   }
+      // }
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      accessTokenUrl: "https://accounts.facebook.com/o/oauth2/token",
+    })
     // ...add more providers here
   ],
+  callbacks: {
+    signIn: async () => {
+      return true
+    },
+
+    async jwt({ token, account }) {
+      console.log("token", token)
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    
+    async session({ session, token, user }) {
+      const sessionWithToken = {
+        ...session,
+        accessToken: String(token.accessToken)
+      }
+      
+      return sessionWithToken
+    }
+  },
 }
+
 export default NextAuth(authOptions)
--- src/components/login/modal_sign_in.js
+++ src/components/login/modal_sign_in.js
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import {Grid, Box} from "theme-ui";
+import { Grid, Box } from "theme-ui";
 import { signIn, signOut, useSession } from "next-auth/react";
 import GoogleButton from "react-google-button";
+import { FacebookLoginButton } from "react-social-login-buttons";
 
-function ModalWrapper(props) {
+function ModalSignIn(props) {
   const [show, setShow] = useState(false);
+
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  // const [session, loading] = useSession();
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const { data: session } = useSession();
+  
 
   return (
     <>
-      {console.log(">>><<>>", session)}
       <Button variant="primary" onClick={handleShow}>
         {props.tile}
       </Button>
@@ -25,29 +29,32 @@ function ModalWrapper(props) {
         backdrop="static"
         keyboard={false}
       >
-           <Grid gap={1} columns={["1fr 1fr 1fr"]}>
+        <Grid gap={1} columns={["1fr 2fr 1fr"]}>
+          
           <Box></Box>
           <Box></Box>
-          <Modal.Header closeButton>
-            <Modal.Title>{props.title}</Modal.Title>
+          <Modal.Header closeButton style={{ margin: "1rem" }}>
+            <Modal.Title></Modal.Title>
           </Modal.Header>
+          
           <Box></Box>
           <Box>
             <GoogleButton
+              textButton="Sign in"
               onClick={() => signIn("google")}
               style={{ marginTop: "1rem" }}
             />
           </Box>
           <Box></Box>
+
           <Box></Box>
           <Box>
-            <Button onClick={() => signIn("facebook")}>
-              Sign in with Facebook
-            </Button>
+            <FacebookLoginButton onClick={() => signIn("facebook")}/>
           </Box>
           <Box></Box>
+
           <Box></Box>
-          <Box></Box>
+           <Box style={{ marginBottom: "4rem" }}></Box>
           <Box></Box>
         </Grid>
       </Modal>
@@ -55,4 +62,4 @@ function ModalWrapper(props) {
   );
 }
 
-export default ModalWrapper;
+export default ModalSignIn;
