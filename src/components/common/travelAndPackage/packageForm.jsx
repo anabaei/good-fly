21ada3ad045f904499767262d1bdc7a3659f682a
anabaei <nabaei17@gmail.com>
2023-05-01 17:45:39 -0700

    add validation and fix auth

--- src/components/common/travelAndPackage/packageForm.jsx
+++ src/components/common/travelAndPackage/packageForm.jsx
@@ -10,30 +10,31 @@ function PackageForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-   console.log("III", session)
-   try {
     
-     const ss = await fetch('http://localhost:3000/api/getToken')
-   } catch (error) {
-    console.log("error", error)
-   }
+    console.log("III", session);
+    try {
+     const  jwtTokenResponse = await fetch("http://localhost:3000/api/getToken");
+     const jwtToken = await jwtTokenResponse.text();
    
+   const data = {session}
     const form = event.target;
     const jsonData = new FormData(form);
+
     
-    try {
-      const response = await fetch('http://localhost:3001/packages', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3001/packages", {
+        method: "POST",
         headers: {
-          Authorization: `Bearer ${accessToken}`
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${jwtToken.replace(/['"]+/g, '')}`
         },
-        body: JSON.stringify(jsonData)
+        body: JSON.stringify({
+          ...jsonData,
+          ...data,
+        }),
       });
-      
+
       const result = await response.json();
       console.log(result);
-      
     } catch (error) {
       console.error(error);
     }
@@ -43,12 +44,20 @@ function PackageForm() {
     <Form style={{ padding: "1rem" }} onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Pick up Location</Form.Label>
-        <Form.Control name="location" type="location" placeholder="Enter 1 Location" />
+        <Form.Control
+          name="location"
+          type="location"
+          placeholder="Enter 1 Location"
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Deliver Location</Form.Label>
-        <Form.Control name="destination" type="location" placeholder="Enter Location" />
+        <Form.Control
+          name="destination"
+          type="location"
+          placeholder="Enter Location"
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicDate">
@@ -61,16 +70,25 @@ function PackageForm() {
           onChange={(e) => setDate(e.target.value)}
         />
       </Form.Group>
-      
+
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check name="willingToPay" type="checkbox" label="Willing to Pay" />
+        <Form.Check
+          name="willingToPay"
+          type="checkbox"
+          label="Willing to Pay"
+        />
       </Form.Group>
-      
+
       <Form.Group className="mb-3" controlId="formComment">
         <Form.Label>More info</Form.Label>
-        <Form.Control name="explanation" as="textarea" placeholder="Including size, Weight of the package" rows={3} />
+        <Form.Control
+          name="explanation"
+          as="textarea"
+          placeholder="Including size, Weight of the package"
+          rows={3}
+        />
       </Form.Group>
-      
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
